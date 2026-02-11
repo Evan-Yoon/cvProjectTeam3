abd9ad100bbbf12e383080a620d7acd4e580532b
User <wlgus1805@naver.com>
2026-02-11 14:10:13 +0900

    feat: initialize Capacitor Android platform with necessary project files and assets.

---
+++ yoonjihyun/UI/userUI/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.team3.walkmate',
+  appName: 'walkmate',
+  webDir: 'dist'
+};
+
+export default config;
