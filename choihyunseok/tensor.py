5d78856b4dcdc9772bb0a2bedf2c03115c40f158
CHOI HYUN SEOK <testwelltest01@gmail.com>
2026-02-11 14:06:29 +0900

    200번 교육시킨 모델 업로드

--- choihyunseok/tensor.py
+++ choihyunseok/tensor.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
-model = YOLO("best.pt")
+model = YOLO("choihyunseok/runs/obb/train/weights/best.pt")
 model.export(format="tflite", imgsz = 1280, int8=True, data='-Braile-Block-Copy-2/data.yaml') # 이미지 양자화
